e386f8a18615a207eab635fbf08345d732fa971d
Ariful2634 <ariful2634@gmail.com>
2023-11-24 21:09:59 +0600

    get feature product data from database

--- index.js
+++ index.js
@@ -1,12 +1,62 @@
 const express = require('express');
 const cors = require('cors');
-const app = express()
+const app  = express();
+require('dotenv').config()
 const port = process.env.PORT || 5000;
 
 // middleware
+
 app.use(cors())
 app.use(express.json())
 
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.stv3jdc.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const featureProductCollection = client.db("hubDB").collection("feature")
+
+    // feature product get
+
+    app.get('/featureProduct', async(req,res)=>{
+        // const filter = req.query;
+        // console.log(filter)
+        // const query = {
+        //     // price: {$lt:100}
+        //     // title: {$regex: filter.search, $options:'i'}
+        // }
+        // const options = {
+        //     sort:{
+        //         timestamp: filter.sort === 'asc' ? 1 : -1,
+        //     }
+        // }
+        const cursor = featureProductCollection.find()
+        const result = await cursor.toArray()
+        res.send(result)
+    })
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 
 app.get('/', (req,res)=>{
